5e5f686605c6057eea70ccb7e7a921dfdb42c82e
supaatt <supaatttadderm@gmail.com>
2026-09-06 20:48:25 +0800

    did qn1

--- NumbersArray.java
+++ NumbersArray.java
@@ -20,6 +20,44 @@ public class NumbersArray {
     }
 
     // Write your methods here
-    
+    public static int findMax(Integer[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public static Integer[] findDuplicates(Integer[] arr) {
+        Map<Integer, Integer> counts = new LinkedHashMap<>();
+        for (int num : arr) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> duplicates = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > 1) {
+                duplicates.add(entry.getKey());
+            }
+        }
+        return duplicates.toArray(new Integer[0]);
+    }
+
+    public static Integer[] findUnique(Integer[] arr) {
+        Map<Integer, Integer> counts = new LinkedHashMap<>();
+        for (int num : arr) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> unique = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() == 1) {
+                unique.add(entry.getKey());
+            }
+        }
+        return unique.toArray(new Integer[0]);
+    }
 }
 
